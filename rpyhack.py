158c8361bedfb344fcd8f2faf9877862db4a9b0d
0day <70134418+Jaisal100@users.noreply.github.com>
2022-11-06 01:38:38 -0500

    Update rpyhack.py

--- rpyhack.py
+++ rpyhack.py
@@ -41,26 +41,17 @@ parse_json3=json.loads(data3)
 ''''''
 
 latest_flight=str(parse_json1['flight_number'])
-print(latest_flight)
 mission_name=parse_json1['name']
-print(mission_name)
 launch_date=parse_json1['date_local'][:10]
 launch_date=launch_date[5:]+"-"+launch_date[:4]
-print(launch_date)
 youtube_video=parse_json1['links']['youtube_id']
 youtube_video="http://www.youtube.com/watch?v="+youtube_video
-print(youtube_video)
 rocket_name=parse_json2['name']
-print(rocket_name)
 rocket_success_rate=parse_json2['success_rate_pct']
 rocket_success_rate=('{:}%'.format(rocket_success_rate))
-print(rocket_success_rate)
 launchpad_name=parse_json3['full_name']
-print(launchpad_name)
 launchpad_success_rate=int(((int(parse_json3['launch_successes'])/int(parse_json3['launch_attempts']))*100))
 launchpad_success_rate="{:}%".format(launchpad_success_rate)
-print(launchpad_success_rate)
-
 
 # Launch
 f1=open('f1.txt','w')
